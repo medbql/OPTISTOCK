ca997df86531ec587d6a9bbf74478c8b7ecce816
medbql <hsbintr2@gmail.com>
2026-03-19 10:15:45 +0000

    install front depencecies & desing DB

---
+++ backend/database/migrations/2026_03_19_093911_create_mouvements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mouvements', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type',['entrer','sortie']);
+            $table->integer('quantite');
+            $table->date('date');
+            $table->unsignedBigInteger('produit_id');
+            $table->unsignedBigInteger('fournisseur_id')->nullable();
+            $table->unsignedBigInteger('utilisateur_id');
+            $table->foreign('produit_id')->references('id')->on('produits');
+            $table->foreign('fournisseur_id')->references('id')->on('fournisseurs');
+            $table->foreign('utilisateur_id')->references('id')->on('utilisateur');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mouvements');
+    }
+};
